50c7a4562c8c80ca599f48cac0a7c1d2be7321d1
Adrian Andersen <adrianandersen@protonmail.com>
2022-04-10 16:24:57 +0200

    feat: improve footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 import ContactInfo from "./ContactInfo";
-import { Card } from "@mui/material";
+import { Card, Link } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import CopyrightIcon from "@mui/icons-material/Copyright";
 import moment from "moment";
+import NextLink from "next/link";
+import React from "react";
+import Box from "@mui/material/Box";
+import Image from "next/image";
 
 export default function Footer() {
   return (
@@ -16,6 +20,34 @@ export default function Footer() {
       }}
     >
       <ContactInfo />
+      <Box sx={{ marginBottom: ".2rem" }}>
+        <NextLink href="/info/policies/conditions" passHref>
+          <Link variant="body2" underline="none">
+            Betingelser
+          </Link>
+        </NextLink>
+        {" | "}
+        <NextLink href="/info/policies/terms" passHref>
+          <Link variant="body2" underline="none">
+            Vilkår
+          </Link>
+        </NextLink>
+        {" | "}
+        <NextLink href="/info/policies/privacy" passHref>
+          <Link variant="body2" underline="none">
+            Personvernserklæring
+          </Link>
+        </NextLink>
+      </Box>
+      <Image
+        width={200}
+        height={72}
+        src="/DIBS_shop_vertical_EN_10.png"
+        alt="Dibs easy logo"
+      />
+      <Typography sx={{ marginTop: ".2rem" }}>
+        Organisasjonsnummer: 912047385 MVA
+      </Typography>
       <Typography sx={{ display: "flex", gap: ".4rem" }}>
         Boklisten.no AS
         <CopyrightIcon />
