fa739e56c582ed8dc8d3969dad2134792b7b29c9
Lars Mitsem Selbekk <lars@selbekk.com>
2023-06-14 01:33:19 +0200

    refactor(theme): add typography variants

---
+++ src/utils/typographyVariants.ts
@@ -0,0 +1,32 @@
+/**
+ * This file extends the TS definitions for MUI typography to our new variants.
+ * @see theme
+ */
+import React from "react";
+// used for module JSdoc
+// eslint-disable-next-line no-unused-vars
+import theme from "./theme";
+
+declare module "@mui/material/styles" {
+  // eslint-disable-next-line no-unused-vars
+  interface TypographyVariants {
+    cardHeader: React.CSSProperties;
+    title: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  // eslint-disable-next-line no-unused-vars
+  interface TypographyVariantsOptions {
+    cardHeader?: React.CSSProperties;
+    title?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  // eslint-disable-next-line no-unused-vars
+  interface TypographyPropsVariantOverrides {
+    cardHeader: true;
+    title: true;
+  }
+}
