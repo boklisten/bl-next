eaa288791fff911e17467c88439f68be72c7448e
Lars Mitsem Selbekk <lars@selbekk.com>
2023-06-19 01:05:09 +0100

    chore(MeetingInfo): add icon beside meeting location

--- src/components/matches/MeetingInfo.tsx
+++ src/components/matches/MeetingInfo.tsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material";
 import ScheduleIcon from "@mui/icons-material/Schedule";
 import { formatDatetime } from "./matchesList/helper";
 import React from "react";
+import PlaceIcon from "@mui/icons-material/Place";
 import { MatchWithDetails } from "@boklisten/bl-model";
 
 const MeetingInfo = ({ match }: { match: MatchWithDetails }) => {
@@ -10,9 +11,12 @@ const MeetingInfo = ({ match }: { match: MatchWithDetails }) => {
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "left" }}>
-      <Typography fontWeight="bold" variant={"subtitle1"}>
-        {meetingLocation}
-      </Typography>
+      <Box sx={{ display: "flex", marginTop: ".2rem", alignItems: "center" }}>
+        <PlaceIcon sx={{ marginRight: ".2rem" }} />
+        <Typography fontWeight="bold" variant={"subtitle1"}>
+          {meetingLocation}
+        </Typography>
+      </Box>
       <Box sx={{ display: "flex", marginTop: ".2rem", alignItems: "center" }}>
         <ScheduleIcon sx={{ marginRight: ".2rem" }} />
         {(meetingTime && (
